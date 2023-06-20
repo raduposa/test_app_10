a6db6184ac1fe68cea108514605724d36fd0ff1d
Radu Posa <posaradu.cp@gmail.com>
2023-06-20 12:26:50 +0300

    Initial commit and add root route

---
+++ ruby_projects/ruby.rb
@@ -0,0 +1,18 @@
+users = [
+    {username: "radu", password: "password1"}
+    {username: "jack", password: "password2"}
+    {username: "anya", password: "password3"}
+    {username: "john", password: "password4"}
+    {username: "luka", password: "password5"}    {username: "jack", password: "password2"}
+
+]
+puts "Welcome to the authenticator"
+25.times {print "-"}
+puts
+puts "This program will take input from the user and compare passwrd"
+puts "If password is correct, you will get back the user object"
+
+
+while true 
+puts "This will keep printing"
+end
